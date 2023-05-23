16c82bc64ba98999f1fe7181e133e39d0a9cfd4d
LouisonFvrs <louison.feuvrais@gmail.com>
2023-05-23 14:46:25 +0200

    commandes pour définir les thèmes

--- js/terminal.js
+++ js/terminal.js
@@ -6,6 +6,21 @@ let result = document.getElementById("result");
 let container = document.getElementById("scroll");
 let tabCommand = ["aide", "get-cv", "get-linkedin", "get-github", "information", "get-projet", "theme"];
 
+/*
+    fonction permettant de changer le thème du terminal
+*/
+function changeTheme(n)
+{
+    n-=1;
+    tab = [['#3b3b3b','#ffffff','#e73c7e','#23a6d5'],['#FAFAFA','#A626A4','#526FFF','#50A14F'],['#282a36','#f8f8f2','#ff5555','#50fa7b'],['#0a0047','#f8f8f2','#ff4499','#00ffd2'],['#2C001E','#FFFFFF','#E95420','#772953']];
+
+    document.documentElement.style.setProperty('--main-bg-color', tab[n][0]);
+    document.documentElement.style.setProperty('--main-text-color', tab[n][1]);
+    document.documentElement.style.setProperty('--important-span', tab[n][2]);
+    document.documentElement.style.setProperty('--highlight-text', tab[n][3]);
+
+}
+
 leave.addEventListener("click", ()=> {
     if(getComputedStyle(popup).display != 'none'){
         popup.style.display = 'none';
@@ -79,9 +94,35 @@ document.addEventListener("keydown", function(event) {
         /*
             Commande get-projet
         */
-       if(inputValue == 'get-projet'){
-        result.innerHTML += '<br><table><thead><tr><th width = "10%">Projet</th><th>Outils</th><th width = "10%">Team</th><th>Description</th><th width = "10%">Voir le projet</th></tr></thead><tbody><tr><td>Gestion étudiant</td><td>Java, JavaFx, SQL, Figma</td><td>Oui</td><td>Ce projet est une application lourde de gestion d&apos;étudiant, on peut visualiser, en fonction de vos permissions lors de votre connexion vous pourrez créer modifier, supprimer des étudiants</td><td><a href="#">Projet</a></td></tr></thead><tbody><tr><td>BoisDuRoy</td><td>HTML, CSS, JS, PHP, SQL, C#, Figma, Git</td><td>Oui</td><td>Le but est de pouvoir visualiser ses informations et effectuer une demande de congé sur une appli web qui pourra être accepter ou non via ld&apos;appli c#</td><td><a href="https://github.com/LouisonFvrs/BoisDuRoy">Projet</a></td></tr><tr><td>Snake</td><td>C#</td><td>Non</td><td>L&apos;objectif était de reproduire le jeu snake afin de me familiariser avec le language C#</td><td><a href="https://github.com/LouisonFvrs/Snake">Projet</a></td></tr><tr><td>Portfolio Terminal</td><td>HTML, CSS, JS</td><td>Non</td><td>Souhaitant réaliser un portfolio un peu original j&apos;ai eu l&apos;idée de réaliser un site web reproduisant visuelement un portfolio mais en y implémentant les fonctionnalités que peux posséder un terminal</td><td><a href="https://github.com/LouisonFvrs/Portfolio">Projet</a></td></tr></tbody></table><br>';
+       if(inputValue == 'get-projet')
+       {
+        result.innerHTML += '<br><table><thead><tr><th width = "10%">Projet</th><th>Outils</th><th width = "10%">Team</th><th>Description</th><th width = "10%">Voir le projet</th></tr></thead><tbody><tr><td>Gestion étudiant</td><td>Java, JavaFx, SQL, Figma</td><td>Oui</td><td>Ce projet est une application lourde de gestion d&apos;étudiant, on peut visualiser en fonction de ses permissions attribuer lors de votre connexion, vous pourrez créer, modifier, supprimer des étudiants.</td><td><a href="#" target="_blank">Projet</a></td></tr></thead><tbody><tr><td>BoisDuRoy</td><td>HTML, CSS, JS, PHP, SQL, C#, Figma, Git</td><td>Oui</td><td>Le but est de pouvoir visualiser ses informations et effectuer une demande de congé sur une appli web, ces demandes devront être accepter ou non via ld&apos;appli c#.</td><td><a href="https://github.com/LouisonFvrs/BoisDuRoy" target="_blank">Projet</a></td></tr><tr><td>Snake</td><td>C#</td><td>Non</td><td>L&apos;objectif était de reproduire le jeu snake afin de me familiariser avec le language C#.</td><td><a href="https://github.com/LouisonFvrs/Snake" target="_blank">Projet</a></td></tr><tr><td>Portfolio Terminal</td><td>HTML, CSS, JS</td><td>Non</td><td>Souhaitant réaliser un portfolio un peu original j&apos;ai eu l&apos;idée de réaliser un site web reproduisant visuelement un portfolio mais en y implémentant les fonctionnalités que peux posséder un terminal. De plus ce portfolio est quant à lui responsive, il peut donc être visiter via un smartphone ou une tablette.</td><td><a href="https://github.com/LouisonFvrs/Portfolio" target="_blank">Projet</a></td></tr></tbody></table><br>';
        }
+
+       /*
+            Commande thème
+        */
+       if(inputValue == 'theme' || inputValue == 'thème' || inputValue == 'themes' || inputValue == 'thèmes')
+       {
+        result.innerHTML += '<br>Voici la liste des thèmes disponibles :<br><br><i>{<br>&nbsp;&nbsp;- Defaut;<br>&nbsp;&nbsp;- Atom;<br>&nbsp;&nbsp;- Dracula;<br>&nbsp;&nbsp;- Tokyo;<br>&nbsp;&nbsp;- Ubuntu;<br>}</i><br><br>Entrer la commande suivante pour définir votre thème : theme.Nom<br>';
+       }
+
+        if(inputValue.toUpperCase() == 'THEME.DEFAUT'){
+            changeTheme(1);
+        }
+        if(inputValue.toUpperCase() == 'THEME.ATOM'){
+            changeTheme(2);
+        }
+        if(inputValue.toUpperCase() == 'THEME.DRACULA'){
+            changeTheme(3);
+        }
+       if(inputValue.toUpperCase() == 'THEME.TOKYO'){
+            changeTheme(4);
+        }
+        if(inputValue.toUpperCase() == 'THEME.UBUNTU'){
+            changeTheme(5);
+        }
+
 
        //   Gestion de la barre de scrolling du terminal pour quelle reste tout en bas
 
